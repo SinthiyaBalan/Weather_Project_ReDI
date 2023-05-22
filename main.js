87da946ea19be048c82c1cbc6c07aa9213bb779c
SinthiyaBalan <sinthiyamct@gmail.com>
2023-05-22 14:10:41 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+const ul = document.createElement('ul')
+document.body.appendChild(ul)
+console.log("hi")
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=munich&appid=e10c8842896652d1ca26c5176f602a72")
+.then(i => i.json())
+.then(i => {
+
+    const li = document.createElement('li');
+ 
+    console.log(i);
+
+    console.log(i.main.temp)
+    console.log(i.name)
+    console.log(i.sys.country);
+    
+    
+   
+  
+
+   
+
+    li.textContent = `City Name :${i.name} ,Temp :${i.main.temp} , Country :${i.sys.country} , Weather : ${i.weather[0].main}`;
+
+  
+    ul.appendChild(li);
+
+    //   i.weather[0].forEach(element => {
+    //     console.log(element.main)
+    //   });
+        
+  })
+
+
+.catch(e => console.log(e))
