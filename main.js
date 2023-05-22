d19d57c10a39ceddd68d8f624d5c77e897185187
SinthiyaBalan <sinthiyamct@gmail.com>
2023-05-22 14:50:58 +0200

    added prompt

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 
 const ul = document.createElement('ul')
 document.body.appendChild(ul)
-console.log("hi")
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=munich&appid=e10c8842896652d1ca26c5176f602a72")
+
+const UserCity = prompt("Enter the city name")
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${UserCity}&appid=e10c8842896652d1ca26c5176f602a72`)
 .then(i => i.json())
 .then(i => {
 
@@ -16,7 +18,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=munich&appid=e10c884289
     console.log(i.name)
     console.log(i.sys.country);
     
-    li.textContent = `City Name :${i.name} ,Temp :${i.main.temp} , Country :${i.sys.country} , Weather : ${i.weather[0].main}`;
+    li.textContent = `City :${i.name} ,Temp :${i.main.temp} , Country :${i.sys.country} , Weather : ${i.weather[0].main}`;
 
   
     ul.appendChild(li);
